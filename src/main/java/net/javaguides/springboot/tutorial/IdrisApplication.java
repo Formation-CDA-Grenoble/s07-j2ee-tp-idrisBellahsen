1afecd6eb02d6eecdff5a705d385ae3bdcead5f2
idrisBellahsen <39699663+idrisBellahsen@users.noreply.github.com>
2020-01-30 11:36:23 +0100

    fin

---
+++ src/main/java/net/javaguides/springboot/tutorial/IdrisApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdrisApplication.class, args);
+	}
+
+}
